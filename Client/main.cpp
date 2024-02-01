83459efbf7de9fa54cff5762c96841990e6cbe5a
lucky634 <khoa21102003@gmail.com>
2024-02-01 11:17:10 +0700

    Add client and server

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "Client.h"
 
 int main()
 {
+	Client client;
+	if (!client.InitializeWsa())
+	{
+		return 1;
+	}
+	if (!client.InitializeSocket())
+	{
+		return 2;
+	}
+	if (!client.Connect("127.0.0.1", 8080))
+	{
+		return 3;
+	}
+	client.SendData("Hello");
+	system("pause");
 	return 0;
 }
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include "Server.h"
 
 int main()
 {
+	Server server;
+	if (!server.InitializeWsa())
+	{
+		return 1;
+	}
+	if (!server.InitializeSocket())
+	{
+		return 2;
+	}
+	if (!server.Bind(8080))
+	{
+		return 3;
+	}
+	if (!server.Listen())
+	{
+		return 4;
+	}
+	if (!server.Accept())
+	{
+		return 5;
+	}
+	char buffer[1024] = { 0 };
+	server.ReceiveData(buffer);
+	std::cout << buffer << std::endl;
+	system("pause");
 	return 0;
 }
