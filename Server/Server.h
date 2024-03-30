c0ce639c7fa889208e3e328747dd0ac6485ca465
lucky634 <khoa21102003@gmail.com>
2024-03-30 18:24:38 +0700

    Update

--- Server/Server.h
+++ Server/Server.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <thread>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+#include <bcrypt.h>
 
 #pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "bcrypt.lib")
 
 #define MAX_BUFFER_SIZE 1024
 
@@ -12,21 +19,32 @@ class Server
 public:
 	Server();
 	virtual ~Server();
-	bool InitializeWsa();
-	bool InitializeSocket();
-	bool Bind(int port);
-	bool Listen();
-	bool Accept();
-	bool SendData(const char* buffer);
-	bool ReceiveData(char* buffer);
+	void Run(int port = 8080);
 
 private:
-	WSAData m_wsaData;
-	int m_port;
-	SOCKET m_socket;
-	SOCKADDR_IN m_socketAddress;
+	bool InitWsa();
+	void CollectError();
+	bool CreateSocket();
+	bool BindSocket(int port = 8080);
+	bool ListenSocket();
+	bool AcceptClient();
+	void CleanupWsa();
+	void CloseSocket();
+	bool Authenticate();
+	void Load(const std::string& path);
+	void HandleClient();
+	void SendMessage(std::string msg);
+	bool PerformKeyExchange();
 
+private:
+	WSADATA m_wsaData;
+
+	SOCKET m_serverSocket;
 	SOCKET m_clientSocket;
-	SOCKADDR_IN m_clientSocketAddress;
+
+	std::vector<char> m_sharedKey;
+
+	std::map<std::string, std::string> m_users;
+	std::thread m_thread;
 };
 
